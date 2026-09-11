7b832fd6fae50342e19d547c6a1efb3195d88b4e
anandukrrishna <anandukrrishna@gmail.com>
2026-09-11 14:30:39 +0530

    Initial Django project setup with user registration API

---
+++ accounts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    referral_code = models.CharField(
+        max_length=20,
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    referred_by = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='referrals'
+    )
+
+    referral_position = models.CharField(
+        max_length=5,
+        choices=[
+            ('LEFT', 'Left'),
+            ('RIGHT', 'Right'),
+        ],
+        null=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.referral_code:
+            self.referral_code = str(uuid.uuid4()).replace('-', '')[:10].upper()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
